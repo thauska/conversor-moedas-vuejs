122c5634b1aa2496551661e51d47737e20c751b2
Thayana Mamoré <thayana.mamore@topi.io>
2021-01-11 14:17:53 -0300

    Configurando corretamente para deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-yarn build
+yarn run build
 
 # navigate into the build output directory
 cd dist
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:thauska/conversor-moedas-vuejs.git main:gh-pages
+git push -f git@github.com:thauska/conversor-moedas-vuejs.git master:gh-pages
 
 cd -
